545764e249d7ab817947f229a37e98dfb221a7d3
aouiniamine <amine.m.aouini@hotmail.com>
2024-07-13 23:10:27 +0100

    setup template and email list

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const html =
+`<body style="color: black;">
+    <p>Dear Recruiter,</p>
+    <p>
+        I'm Amine, a Full Stack Developer and a professional problem solver,
+        with a track record working with international companies,
+        Where I've made numerous initiatives in scaling and building robust solutions and managing teams.
+    </p>
+    <p>
+        I'm Interested in your company, and I'd love to take on a new journey within your team.<br>
+        <a style="color: blue;" href="https://drive.google.com/file/d/1I280aaGOdszOJZecu_cb5XqAhnVHgxC7/view?usp=drive_link">Here's my Email</a>
+    </p>
+    <p>If my profile corresponds with any position within your team, please let me know.<br>
+    Hope you have a great day :)</p>
+
+    <p>
+        Amine M. Aouini <br>
+        Full Stack Developer <br> 
+        amine.m.aouini@hotmail.com <br>
+        216-26-115-971
+    </p>
+</body>`
+
+
+const emailList = [
+];
+
+module.exports = {html, emailList}
