90b87aaad30158f67884bb579af06a1cd6fb7dde
aouiniamine <amine.m.aouini@hotmail.com>
2024-07-14 01:17:19 +0100

    fix explanation

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const {emailList} = require("./src/data")
 // Warning: don't promise all it'll give you 'Concurrent connections limit exceeded' Error
 
 // Explenation to the code:
-// making a for loop with setTimeout won't make each one executes after 1.7s.
+// making a for loop with setTimeout acts like Promise.all
 // so i made a recusive func to function as it should.
+// sending each email after 1.7s
 
 let timeoutNumber = 1700
 let i = 0
